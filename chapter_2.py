1e51f6af1e202b26ed569d3a77348e2938476b56
Mark Mote <137040517+markmote1@users.noreply.github.com>
2025-06-24 20:38:20 +0300

    Created chapter instructions and began the creation of A Vocab builder

---
+++ chapter_2.py
@@ -0,0 +1,27 @@
+
+''' 1. Take definitions.json, read it in as our data, and use a spaced repetition algorithm 
+to create a long list where each word is encoutered less and less often, thus increasing the spacing
+between encounters as we strengthen our knowledge of the word.'''
+
+# Libraries imports
+
+import json # Importing the json module to read JSON files-it will read the definitions file
+import csv #  reading and writing CSV files
+
+
+
+
+'''# 1.1 is to read the definitions file and process it
+
+# 1.2 create a complex object that keeps track of a whole lot of properties about the word or attribute
+ / therefore we need to define a class for this object
+1.2.1 were going to need to define a bunch of methods(functions specific to a class that are like kind of actions- such as a method, that we can use to automatically handle a word encounter)
+
+  1.3 Instantiate this class/object for every word
+
+  1.4 We can create the spaced repetition algorithms that we'll use to 
+  create this long list that clerverly introduces new words, and tests old words at the appropriate 
+  intervals
+
+  1.5 save this long list of all our words to a csv file that can later be read by our game
+ '''
---
+++ play.py
@@ -0,0 +1,14 @@
+'''2. Create a game, where a user has to get through as many items in 
+the list (i.e. answer them correctly, and when they ultimately fail, then they get
+a high score which also forces them to continue practicing the words)
+
+2.1 is to create a functions to read both the long list of space-repeated
+words so that we can loop over them and test the user continously until they fail, at which point
+we give them a score, and also definitions so that we may show a user a word definition
+if its their first time encountering the word.
+
+#2.2 define a continous while loop that tests the user
+on each word inthe list, one after the other in the provided order, and will only
+exit when each word has been mastered, or the user makes a mistake
+2.3 print the new high score when they fail or win, and also a command to start again
+'''
